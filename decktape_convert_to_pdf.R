cb9081ae883cd57b801379fdd55212993bb63425
Jessica Minnier <jessica.minnier@gmail.com>
2018-02-16 14:22:09 -0800

    add pdf making code

---
+++ decktape_convert_to_pdf.R
@@ -0,0 +1,5 @@
+# convert html to pdf for uploading and printing (gifs don't work)
+
+# via https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF
+
+system("decktape remark 2018_02_Shiny_CSP/minnier_csp2018.html 2018_02_Shiny_CSP/minnier_csp2018.pdf")
