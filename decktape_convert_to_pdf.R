9af583c0cb927995b065ff683fe0497f98a8c84c
Jessica Minnier <jessica.minnier@gmail.com>
2018-07-28 21:15:52 -0700

    jsm convert to pdf

--- decktape_convert_to_pdf.R
+++ decktape_convert_to_pdf.R
@@ -3,3 +3,7 @@
 # via https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF
 
 system("decktape remark 2018_02_Shiny_CSP/minnier_csp2018.html 2018_02_Shiny_CSP/minnier_csp2018.pdf")
+
+system("decktape remark 2018_07_JSM_InteractiveLearning/minnier_jsm2018.html 2018_07_JSM_InteractiveLearning/minnier_jsm2018.pdf")
+webshot(here::here("2018_07_JSM_InteractiveLearning","minnier_jsm2018.html"),
+          file = here::here("2018_07_JSM_InteractiveLearning","minnier_jsm2018.pdf"))
