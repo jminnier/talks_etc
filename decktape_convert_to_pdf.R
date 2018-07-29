1a1bf2b6146cc331211ba3a423e19896a7d4c170
Jessica Minnier <jessica.minnier@gmail.com>
2018-07-29 14:31:26 -0700

    update html so pdf works for jsm

--- decktape_convert_to_pdf.R
+++ decktape_convert_to_pdf.R
@@ -4,6 +4,7 @@
 
 system("decktape remark 2018_02_Shiny_CSP/minnier_csp2018.html 2018_02_Shiny_CSP/minnier_csp2018.pdf")
 
-system("decktape remark 2018_07_JSM_InteractiveLearning/minnier_jsm2018.html 2018_07_JSM_InteractiveLearning/minnier_jsm2018.pdf")
+system("decktape remark 2018_07_JSM_InteractiveLearning/minnier_jsm2018.html 2018_07_JSM_InteractiveLearning/minnier_jsm2018_2.pdf")
 webshot(here::here("2018_07_JSM_InteractiveLearning","minnier_jsm2018.html"),
           file = here::here("2018_07_JSM_InteractiveLearning","minnier_jsm2018.pdf"))
+# notes, did not like the `` text, images or plots that were cut off on the html slides did not show up on pdf, neither with Chrome print to pdf
